8c8140f505da1b330395d6a94d38a1c8742a22b5
Sirjan Singh Bhasin <sirjansingh57@gmail.com>
2024-10-23 08:27:07 +0530

    Update remote_backend_s3.tf

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,7 +1,7 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "sirjan-proj-remote-state-bucket-123456"
     key    = "devops-project-1/terraform.tfstate"
     region = "eu-central-1"
   }
-}
+}*/
